dc137c440532a11e338a30522b6ad17c262dd8c2
kgoransika <kgoransika@students.nsbm.ac.lk>
2023-01-30 14:03:36 +0530

    -testing

--- client/src/app/board-event-holder/board-event-holder.component.spec.ts
+++ client/src/app/board-event-holder/board-event-holder.component.spec.ts
@@ -1,12 +1,15 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { BoardEventHolderComponent } from './board-event-holder.component';
+import { EventService } from '../_services/event.service';
 
 describe('BoardEventHolderComponent', () => {
   let component: BoardEventHolderComponent;
   let fixture: ComponentFixture<BoardEventHolderComponent>;
+  let httpMock: HttpTestingController;
+  let service: EventService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,6 +23,8 @@ describe('BoardEventHolderComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(BoardEventHolderComponent);
+    service = TestBed.inject(EventService);
+    httpMock = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- client/src/app/login/login.component.spec.ts
+++ client/src/app/login/login.component.spec.ts
@@ -1,14 +1,17 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { validUser } from 'src/mocks';
+import { AuthService } from '../_services/auth.service';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,6 +25,8 @@ describe('LoginComponent', () => {
     .compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
+    httpMock = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(AuthService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -40,15 +45,28 @@ describe('LoginComponent', () => {
     expect(component.form.valid).toBeFalsy();
   })
 
-    it('username field validity', () => {
-      const username = fixture.debugElement.query(By.css('username'));
-      const loginBtn = fixture.debugElement.nativeElement.querySelector('button');
-      loginBtn.click();
-    })
+  it('username field validity', () => {
+    const username = fixture.debugElement.query(By.css('username'));
+    expect(username).toBeFalsy();
+  })
 
-    it('password field validity', () => {
-      const password = fixture.debugElement.query(By.css('password'));
-      expect(password).toBeFalsy();
-    })
+  it('password field validity', () => {
+    const password = fixture.debugElement.query(By.css('password'));
+    expect(password).toBeFalsy();
+  })
 
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should login user', () => {
+
+    service.login(validUser.username,validUser.password).subscribe((response) => {
+      expect(response).toEqual({ success: true });
+    });
+
+    const req = httpMock.expectOne('http://localhost:8080/api/auth/signin');
+    expect(req.request.method).toBe('POST');
+     req.flush({ success: true });
+    });
 });
